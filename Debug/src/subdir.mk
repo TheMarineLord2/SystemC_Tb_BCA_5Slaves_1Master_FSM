3e03e7ef15bd25505155f160498a5a87de80af4d
TheMarineLord2 <jbullmann10+github@gmail.com>
2026-06-03 16:22:45 +0200

    CleanUp

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -4,19 +4,13 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/EclipsePr.cpp \
-../src/TrafficLight.cpp \
-../src/library.cpp 
+../src/EclipsePr.cpp 
 
 CPP_DEPS += \
-./src/EclipsePr.d \
-./src/TrafficLight.d \
-./src/library.d 
+./src/EclipsePr.d 
 
 OBJS += \
-./src/EclipsePr.o \
-./src/TrafficLight.o \
-./src/library.o 
+./src/EclipsePr.o 
 
 
 # Each subdirectory must supply rules for building sources it contributes
@@ -31,7 +25,7 @@ src/%.o: ../src/%.cpp src/subdir.mk
 clean: clean-src
 
 clean-src:
-	-$(RM) ./src/EclipsePr.d ./src/EclipsePr.o ./src/TrafficLight.d ./src/TrafficLight.o ./src/library.d ./src/library.o
+	-$(RM) ./src/EclipsePr.d ./src/EclipsePr.o
 
 .PHONY: clean-src
 
--- src/library.h
+++
@@ -1,6 +0,0 @@
-#ifndef PROJEKT_LIBRARY_H
-#define PROJEKT_LIBRARY_H
-
-void hello();
-
-#endif // PROJEKT_LIBRARY_H
